1ff1c776876d9769635a1aa638d5532f306769a2
Gurminder Boparai <gurminder_b100@hotmail.com>
2026-08-29 14:36:57 -0700

    feat: configure development and production environment files for project settings

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,29 +1,8 @@
 export const environment = {
-  production: true,
-  // apiUrl: 'http://bloomvieapis.taskwrap.com/api',
-  // Cpanel: 'http://bloomviecpanel.taskwrap.com/',
-  // frontEndWebUrl: 'http://bloomvie.taskwrap.com/',
-
-
-  // AWS 
-  // Cpanel: 'https://stagingcpanel.bloomvie.com/',
-  // frontEndWebUrl: 'https://staging.bloomvie.com/',
-
-  // AWS
-
-
-  // Bloomvie 
-  // apiUrl: 'https://apis.bloomvie.com/api',
-  // apiUrl: 'https://stagingapis.bloomvie.com/api',
-  Cpanel: 'https://cpanel.bloomvie.com/',
-  frontEndWebUrl: 'https://www.bloomvie.com/',
-  cookiesDomain: '.bloomvie.com',
-
-  // Bloomvie 
-
-
+  production: false,
   apiUrl: 'http://localhost:5065/api',
-
-
-  secretKey: 'encrypt!135790',
+  Cpanel: 'http://localhost:5067/',
+  frontEndWebUrl: 'http://localhost:5066/',
+  cookiesDomain: 'localhost',
+  secretKey: 'encrypt!135790'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,28 +1,8 @@
 export const environment = {
-  production: false,
-  // AWS 
-
-
-  // apiUrl: 'http://sprint01apis.bloomvie.com/api',
-  // Cpanel: 'http://sprint01cp.bloomvie.com/',
-  // frontEndWebUrl: 'http://sprint01.bloomvie.com/',
-
-  // apiUrl: 'https://stagingapis.bloomvie.com/api',
-  // Cpanel: 'https://stagingcpanel.bloomvie.com/',
-  // frontEndWebUrl: 'https://staging.bloomvie.com/',
-
-
-  // Bloomvie 
-  // apiUrl: 'https://apis.bloomvie.com/api',
+  production: true,
+  apiUrl: 'https://apis.bloomvie.com/api',
   Cpanel: 'https://cpanel.bloomvie.com/',
   frontEndWebUrl: 'https://www.bloomvie.com/',
   cookiesDomain: '.bloomvie.com',
-  // Bloomvie 
-
-
-  // AWS
-  apiUrl: 'http://localhost:5065/api',
-  // Cpanel: 'http://bloomviecpanel.taskwrap.com/',
-  // frontEndWebUrl: 'http://bloomvie.taskwrap.com/',
-
+  secretKey: 'encrypt!135790'
 };
