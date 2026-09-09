b8bd6556ba1e202c4a5281263113e858ef51af85
Gurminder Boparai <gurminder_b100@hotmail.com>
2026-09-08 22:15:02 -0700

    add new build

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://stagingapis.bloomvie.com/api',
+  Cpanel: 'https://stagingcpanel.bloomvie.com/',
+  frontEndWebUrl: 'https://staging.bloomvie.com/',
+  cookiesDomain: '.bloomvie.com',
+  secretKey: 'encrypt!135790',
+  stripePublishableKey: 'pk_test_51REatbADi5PePYyrsmnVeBawb0mtDjznD0DmlvFlejvJ0cZGJjc1CohNZdc3Dw0UovQyD2vMlU8vZ1BtPAsyqEFg00tYNBrGlb'
+};
